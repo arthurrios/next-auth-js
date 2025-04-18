914d5ea577e9cbf6beca98abbf9f16a6b90977ff
arthur <arthur.rios007@gmail.com>
2025-04-18 13:53:33 -0300

    feat: add sign out action

---
+++ src/app/dash/_components/AppBar.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { signOutAction } from '../_actions/signOutAction'
+
+export function AppBar() {
+  return (
+    <header className="h-20 flex justify-end border-b items-center px-6">
+      <Button onClick={signOutAction}>Log out</Button>
+    </header>
+  )
+}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import { db } from './db'
 import { compare } from 'bcryptjs'
 import { loginSchema } from '@/schemas/loginSchema'
 
-export const { signIn, auth } = NextAuth({
+export const { signIn, signOut, auth } = NextAuth({
   session: {
     strategy: 'jwt',
   },
